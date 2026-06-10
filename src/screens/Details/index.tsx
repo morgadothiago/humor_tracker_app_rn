c6df305a20ea1cc315c3e9af1813944ffe7b1a1e
Thiago Morgado <thiago_morgado@hotmail.com>
2026-06-10 01:03:45 -0300

    feat: remover navegação e parâmetros não utilizados nas telas Details e SetUserNameDetails

--- src/screens/Details/index.tsx
+++ src/screens/Details/index.tsx
@@ -2,15 +2,14 @@ import { Text, View } from "react-native";
 import { styles } from "./styles";
 
 
-import { useNavigation, useRoute } from "@react-navigation/native";
-import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { useRoute } from "@react-navigation/native";
+
 import { TRouteProps } from "@/routes/Stack";
 
 
-type TNavigationProps = NativeStackNavigationProp<Record<string, any>>;
+
 
 export const Details = () => {
-  const navigation = useNavigation<TNavigationProps>();
   const { params } = useRoute<TRouteProps<'Details'>>();
 
 
--- src/screens/SetUserNameDetails/index.tsx
+++ src/screens/SetUserNameDetails/index.tsx
@@ -1,5 +1,5 @@
 import { Text, View, } from "react-native";
-import { useRoute, } from "@react-navigation/native";
+
 
 
 import { styles } from "./styles";
@@ -7,13 +7,10 @@ import { styles } from "./styles";
 
 
 export const SetUserNamePage = () => {
-  const { params } = useRoute<any>();
-
-
 
   return (
     <View style={styles.container}>
-      <Text>Set User Name Details Screen... {params.id}</Text>
+      <Text>Set User Name Details Screen...</Text>
 
     </View>
   );
