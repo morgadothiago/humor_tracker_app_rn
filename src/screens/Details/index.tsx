8940e87e26b76f43538d4c066121ee62638bced4
Thiago Morgado <thiago_morgado@hotmail.com>
2026-06-10 00:44:21 -0300

    feat: atualizar navegação e exibição de parâmetros nas telas Home e Details

--- src/screens/Details/index.tsx
+++ src/screens/Details/index.tsx
@@ -1,8 +1,8 @@
-import { Pressable, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import { styles } from "./styles";
-import { RootStackParamList } from "@/types/navigation";
 
-import { useNavigation } from "@react-navigation/native";
+
+import { useNavigation, useRoute } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 
 
@@ -10,15 +10,14 @@ type TNavigationProps = NativeStackNavigationProp<Record<string, any>>;
 
 export const Details = () => {
   const navigation = useNavigation<TNavigationProps>();
+  const { params } = useRoute<any>();
 
 
   return (
     <View style={styles.container}>
-      <Text>Details Screen</Text>
+      <Text>Details Screen... {params.rate}</Text>
 
-      <Pressable onPress={() => navigation.navigate('SetUserNamePage', { id: 1 })}>
-        <Text>Go to Set User Name Details</Text>
-      </Pressable>
+
     </View>
   );
 }
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,17 +1,21 @@
 import { Pressable, Text, View } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import { RootStackParamList } from "@/types/navigation";
+
 
 import { styles } from "./styles";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+
+
+type TNavigationProps = NativeStackNavigationProp<Record<string, any>>;
 
 export const Home = () => {
-  const navigation = useNavigation<RootStackParamList>();
+  const navigation = useNavigation<TNavigationProps>();
 
 
   return (
     <View style={styles.container}>
       <Text>Home Screen</Text>
-      <Pressable onPress={() => navigation.navigate('Details')}>
+      <Pressable onPress={() => navigation.navigate('Details', { rate: 3 })}>
         <Text>Go to Details</Text>
       </Pressable>
     </View>
